5db4f6092c3781cc9981672f8e2c2a5f7b0f0793
Chinonye Eze <chinonyevic007@gmail.com>
2024-11-09 01:19:03 +0100

    create route53

--- variable.tf
+++ variable.tf
@@ -116,3 +116,16 @@ variable "ec2_key_pair_name" {
     description = "the name of the ec2 key pair"
     type        = string 
 }
+
+#route53 variables
+variable "domain_name" {
+    default     = "cloudrookie.online"
+    description = "domain name"
+    type        = string 
+}
+
+variable "record_name" {
+    default     = "www"
+    description = "sub domain name"
+    type        = string 
+}
